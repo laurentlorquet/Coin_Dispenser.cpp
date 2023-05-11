3ae6c9508c348e3c2fafd214fa251a9a6df4cae6
Laurent Lorquet <laurentlorquet@gmail.com>
2023-05-11 17:35:25 -0400

    First commit

---
+++ LaurentLorquet_Assignment1.cpp
@@ -0,0 +1,41 @@
+// Assigment 1 
+// The purpose of this program is to run a coin dispenser
+// Author: Laurent Lorquet
+// Date: 09/05/2022 20:57:08
+#include <iostream>
+#include <string>
+#include <math.h>
+using namespace std;
+
+int main(){
+
+// integers (q) represents quarters, (c) for cents, (d) for dimes, (n) for nickels, (p) for pennies
+// cash represents total value in dollars and cents
+float cash, q, c, d, n, p;
+cout << "Please enter the value in dollars and cents (x.xx): $ \n";
+cin >> cash;
+
+// Total cash into cents
+c = cash * 100;
+
+// First larger coin, quarters (q) followed by remaining cents (c)
+q = c / 25;
+c = c% 25;
+
+// Second is dimes (d) followed by remaining cents (c)
+d = c / 10;
+c = c% 10;
+
+// Lastly comes nicles followed by remaining pennies (p)
+n = c / 5;
+p = c% 5;
+
+// Displaying the original dollar amount along with coin designations and their respective number
+cout << "Your change is the following: \n";
+cout << q << " quarters \n";
+cout << d << " dime \n";
+cout << n << " nickels \n";
+cout << p << " pennies \n";
+
+return 0;
+}
